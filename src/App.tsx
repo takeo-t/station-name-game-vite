ce067316b09331c66faa28792fa25168f7843841
takeo-t <takeo-t@118satellite.com>
2025-01-15 22:34:19 +0900

    管理者画面のリンクを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ManageQuestionsPage from './pages/ManageQuestionsPage';
 
@@ -81,9 +81,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <Link to="/">ホーム</Link> | <Link to="/manage-questions">問題管理</Link>
-        </nav>
         <Routes>
           <Route path="/" element={
             <div>
